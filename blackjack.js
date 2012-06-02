eeb079e00aa36e98de8bfd4eb4de7f9c87a8055e
John Borges <jab24@njit.edu>
2012-06-02 18:30:21 -0400

    Added score function

--- blackjack.js
+++ blackjack.js
@@ -4,3 +4,8 @@ var deal = function () {
 };
 var card1 = deal();
 var card2 = deal();
+var score = function () {
+    return card1+card2;
+};
+console.log("You have cards " + card1 + " and " + card2 +
+        " for a score of " + score());
