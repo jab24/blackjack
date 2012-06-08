5d25ffd53880b407c14a04fb8c0b937e0b37864e
John Borges <jab24@njit.edu>
2012-06-08 03:44:14 +0000

    added getter and setter methods

--- blackjack_part2.js
+++ blackjack_part2.js
@@ -1,4 +1,10 @@
 function Card(suit,number){
-	this.suit=suit;
-	this.number=number;
+	this.suit = suit;
+	this.number = number;
+	this.getSuit = function(){
+		return this.getSuit;
+	}
+	this.getNumber = function(){
+		return this.getNumber;
+	}
 }
