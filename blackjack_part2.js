9c7061147ab5fc09daaf6d975bd999e710623c1a
John Borges <jab24@njit.edu>
2012-06-08 03:56:44 +0000

    added deal method

--- blackjack_part2.js
+++ blackjack_part2.js
@@ -1,10 +1,17 @@
-function Card(suit,number){
-	var suit = suit;
-	var number = number;
-	var getSuit = function(){
-		return this.getSuit;
+function Card(s,n){
+	var suit = s;
+	var number = n;
+	this.getSuit = function(){
+		return getSuit;
 	}
 	this.getNumber = function(){
 		return getNumber;
 	}
 }
+var deal = function(){
+	suit = Math.floor(Math.random()*4+1);  
+        number = Math.floor(Math.random()*13+1);
+        return new Card(suit,number);
+}
+var card1 = deal();
+var card2 = deal();
